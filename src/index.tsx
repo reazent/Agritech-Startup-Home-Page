15ab5f7e9af1de793b11f5e15b8de2e88b271c0c
reazent <sumit.verma@reazent.com>
2025-11-16 20:31:28 -0400

    [Magic Patterns] Updated from 'Design Not Showing on GitHub'

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
+import React from 'react';
 import './index.css';
-import React from "react";
-import { render } from "react-dom";
-import { App } from "./App";
-render(<App />, document.getElementById("root"));
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(<App />);
+}
